eee99c3305e80134f98cfa1090ed84e3ec5d81cf
hu1618cq <hu1618cq@my.century.edu>
2018-12-09 17:22:43 -0600

    a little change

--- shoestore-master/js/function.js
+++ shoestore-master/js/function.js
@@ -1,11 +1,59 @@
-$(document).ready(function() {
-    
-  //calling thumbBox will return an array.
-  //this array will be 'img'.
-      $('#fig-all').on('click', changeImg);
-    });
-function changeImg(e){
-      var imgSrc = $(e.target).attr('src');
-      var imgAlt = $(e.target).attr('alt');
-      var imgTitle = $(e.target).attr('title');
-    } 
+
+window.addEventListener("load", new function() {
+    document.getElementById('display-table').innerHTML = loadTable();
+
+    addListeners();
+});
+
+function addListeners() {
+  document.getElementById('shoe').addEventListener("click", function(e){
+    menShoe = document.getElementById('shoe').value;
+    menShoe = menShoe.trim();
+    e.preventDefault();
+    document.getElementById('display-table').innerHTML = loadTable();
+
+  });
+}
+
+function loadTable() {
+  var ptable = "<table>";
+  ptable += getTbody();
+
+  return ptable;
+}
+
+
+function getTbody() {
+  var tBody = "<tbody>";
+
+  if (inputTitle == "") {
+
+    for (var i in menShoes) {
+      tBody += getRow(i);
+    }
+
+  } else {
+    for (var i in paintings) {
+      if (inputTitle != null && inputTitle == menShoes[i].title) {
+        tBody = getRow(i);
+        break;
+      }
+    }
+  }
+
+  tBody += "</tbody>";
+
+  return tBody;
+}
+
+function getRow(index) {
+  var tRow = "<tr>";
+  tRow += "<td><input type=\"checkbox\" name=\"index[]\" value=\""  + index + "\"/></td>";
+  tRow += "<td><img src=\"" + menShoes[index].path + "\" alt=\"" + menShoes[index].infor +  "\" class=\"thumb\"/></td>";
+  tRow += "<td><em>" + menShoes[index].title  + "</em></td>";
+  tRow += "<td>" + menShoes[index].price + "</td>";
+
+  tRow += "</tr>";
+
+  return tRow;
+}
