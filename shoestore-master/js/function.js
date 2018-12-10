d38ecfbd3f923680824a15564c6387b9328bd7c0
Brent Windham <pz9580mc@my.century.edu>
2018-12-09 23:26:59 -0600

    added local storage js

--- shoestore-master/js/function.js
+++ shoestore-master/js/function.js
@@ -21,3 +21,31 @@ function saveShoe(){
 //store the students array in the browser local storage.
 window.localStorage.setItem("store1",JSON.stringify(shoes1));
 }
+
+function addToCartArray(){
+  var shoe;
+
+  if (window.localStorage.getItem("shoeList") === null){
+    shoe = [];
+   }
+   else {
+    shoe = JSON.parse(window.localStorage.getItem("shoeList"));
+   }
+   console.log("Hello");
+
+   var name = document.getElementById("shoename").value;
+   var size = document.getElementById("shoesize").value;
+   var quantity = document.getElementById("quantity").value;
+   var price = document.getElementById("prices").value;
+
+   var shoesStorage =  name+ "$"+
+                      size+ "$"+
+                      quantity+ "$"+
+                      price;
+
+    shoe.push(shoesStorage);
+
+    window.localStorage.setItem("shoeList",JSON.stringify(shoe));
+}
+ 
+
