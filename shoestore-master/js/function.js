1dc6bb7e14f1a277643ee47bc2f13c98ab7671be
hu1618cq <hu1618cq@my.century.edu>
2018-12-10 14:20:25 -0600

    display what we save to local to checkout

--- shoestore-master/js/function.js
+++ shoestore-master/js/function.js
@@ -1,29 +1,7 @@
-function saveShoe(){
-  var shoes = [];
-    
-    var prices = document.querySelector("select").value
-    var shoe = menShoes[0];
 
-
-    shoes.push(shoe); 
-//store the students array in the browser local storage.
-window.localStorage.setItem("store",JSON.stringify(shoes));
-}
-
-function saveShoe(){
-  var shoes1 = [];
-    
-
-    var shoe1 = menShoes[0];
-
-
-    shoes.push(shoe1); 
-//store the students array in the browser local storage.
-window.localStorage.setItem("store1",JSON.stringify(shoes1));
-}
 
 function addToCartArray(){
-  var shoe;
+  var shoe= [];
 
   if (window.localStorage.getItem("shoeList") === null){
     shoe = [];
@@ -31,21 +9,32 @@ function addToCartArray(){
    else {
     shoe = JSON.parse(window.localStorage.getItem("shoeList"));
    }
-   console.log("Hello");
-
-   var name = document.getElementById("shoename");
+  
+   var name = document.getElementById("shoename").value;
    var size = document.getElementById("shoesize").value;
    var quantity = document.getElementById("quantity").value;
-   var price = document.getElementById("prices").value;
+   var price = document.getElementsByTagName("p").value;
 
-   var shoesStorage =  menShoes[0].title+ "$"+
-                      size+ "$"+
-                      quantity+ "$"+
-                      menShoes[0].price;
+
+   var shoesStorage =  name +" " +
+                      size + " " +
+                      quantity+ " " + "$"+
+                      price;
 
     shoe.push(shoesStorage);
 
     window.localStorage.setItem("shoeList",JSON.stringify(shoe));
 }
+
+  var sList = JSON.parse(window.localStorage.getItem("shoeList"));
+
+//now you can access all the students using a loop :
+
+// for (var i in sList) {
+  for ( var i in sList) {
+    var tokens = sList[i]
  
+  document.getElementById("product1").innerHTML = tokens;
+  }
+  
 
