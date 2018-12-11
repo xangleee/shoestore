31480178dc661f538586fd12c9cf68f1cdf58a0d
hu1618cq <hu1618cq@my.century.edu>
2018-12-11 15:47:36 -0600

    sort men shoes

--- shoestore-master/js/filter.js
+++ shoestore-master/js/filter.js
@@ -1,14 +1,16 @@
 function sortTable(){
+
 for (var i in menShoe){
 var Open_table = "<table class=\"men-img\">";
 	var open_row = "<tr>";
-	var img = "<td id=\"product1\"><img  id=\"shoeName\" src=\"images/shoe/menShoe/800502_01.jpg\" width=\"200\" height=\"200\" alt=\"A high end shoe made with the help of Kanye West. The lightweight shoe offers quick agility with it flexable material and lightweight feel.\">";
-	 var title ="<p id=\"title\">YEEZY BOOST V2 ZEBRA</p>";
-	var prices = "<p id=\"price\">$50.00 </p>";
-	var button = "<button id=\"add1\" onclick=\"add1Button()\"><a href=\"view.html\">Add to view</a></button>";
+	var img = "<td id=\"product1\"><img  id=\"shoeName\" src=\"" + menShoe[i].path + "\" width=\"200\" height=\"200\" alt=\"A high end shoe made with the help of Kanye West. The lightweight shoe offers quick agility with it flexable material and lightweight feel.\">";
+	 var title ="<p id=\"title\">" + menShoe[i].title + "</p>";
+	var prices = "<p id=\"price\">" + menShoe[i].price + "</p>";
+	var button = "<button id=\"add1\" onclick=\"add1Button()\"><a href=\"" + menShoe[i].pages + "\">Add to view</a></button>";
 	var close_tr = "</tr>";
 	var	Close_table= "</table>";
 var menTable = Open_table + open_row + img + title + prices + button + close_tr +Close_table ;
+	document.write(menTable);
 }
-document.getElementById("display-table").innerHTML = menTable;
+
 }
