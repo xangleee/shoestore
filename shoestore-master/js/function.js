4eade88d6706a2834be141f0bd6474993b561a10
hu1618cq <hu1618cq@my.century.edu>
2018-12-10 21:40:13 -0600

    display and clear for checkout page

--- shoestore-master/js/function.js
+++ shoestore-master/js/function.js
@@ -1,41 +1,47 @@
 
-
 function addToCartArray(){
   var shoe= [];
-
+  //store data into the local storage
   if (window.localStorage.getItem("shoeList") == null){
     shoe = [];
    }
    else {
     shoe = JSON.parse(window.localStorage.getItem("shoeList"));
    }
-  
+ 
    var name = document.getElementById("shoename").title;
    var size = document.getElementById("shoesize").value;
    var quantity = document.getElementById("quantity").value;
    var price = document.getElementById('prices').textContent;
 
-
-   var shoesStorage = name +" " + "Size: " +
-                      size + " " +"Quantity: " +
-                      quantity+ " "  + "Price: "+ "$"+
-                      price;
+   var shoesStorage = name +"," +
+                      size + "," +
+                      quantity+ ","+
+                      price;+ ","+
 
     shoe.push(shoesStorage);
 
     window.localStorage.setItem("shoeList",JSON.stringify(shoe));
 }
 
-                  /*************************
-                  **display in the checkout*
-                  **************************/
-  var sList = JSON.parse(window.localStorage.getItem("shoeList"));
+                  /*display in the checkout*/
 
-// for (var i in sList) {
-  for ( var i in sList) {
-   
-    sList[i]
-  document.getElementById("product").innerHTML = sList[i];
+  var sList = JSON.parse(window.localStorage.getItem("shoeList"));
+  var shoeList = "";
+
+  for (var i = 0; i < sList.length; i++)  {
+    var token= sList[i].split(",");
+    var titles= (token[0]);
+    var sizes = " Size: " +(token[1]);
+    var quantity = (" Quantity: " + token[2]);
+    var prices = (" Price: " + token[3] + "<br>"+ "<br>");
+    var all = titles + sizes+ quantity + prices;
+
+    shoeList += all;
   }
   
-
+document.getElementById("product").innerHTML = shoeList;
+  
+function Clear(){
+   window.localStorage.removeItem("shoeList");
+}
