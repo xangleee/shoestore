c2c24cb123cc84c6f4a2108f0e2799ae326d2bc5
Xang Lee <43192346+xangleee@users.noreply.github.com>
2018-12-11 15:05:14 -0600

    condensed number of shoes

--- shoestore-master/js/shoeData.js
+++ shoestore-master/js/shoeData.js
@@ -30,32 +30,7 @@ var menShoe=[
 				price: "$45.00",
 				infor: "Emerica Triangle Tread pattern with high-density rubber heel drag pod for maximum grip and durability, and a tongue stabilizing wing for a snug fit if you decide to grind up a rail." 
 
-			},{
-				title:"New Balance 519 Men's Cross-Training Shoes",
-				path: "images/shoe/menShoe/newBalance.jpeg",
-				price: "$80.00",
-				infor: "ABZORB supportive cushioning technology. Has padded tongue and collar. Rubber outsole provides reliable traction."
-
-			},{
-				title:"Nike Run Swift Men's Running Shoes",
-				path: "images/shoe/menShoe/nikeRun.jpg",
-				price: "$70.00",
-				infor: "The men's Nike Run Swift lightweight running shoe is tailored to your foot with highly breathable mesh in the forefoot and heel and tightly woven mesh in the midfoot for support."
-
-			},{
-				title:"Supra Method",
-				path: "images/shoe/menShoe/Supra.jpg",
-				price: "$40.00",
-				infor: "Come equipped with the modern stylings and innovative comfort of the fresh Supra® Method shoe! Athletic mid-top silhouette in a versatile design. Uppers of durable nubuck and mesh materials."
-
-			},{
-				title:"VANS WARD LO SUEDE SNEAKER",
-				path: "images/shoe/menShoe/Vans.jpeg",
-				price: "$50.00",
-				infor: "Hit up the skate park or show off your casual street style in the women's Ward low-top sneaker from Vans."
-
-			}
-		 ]
+			}]
 
 //set the womenShoe in an array for easier table create in funciton
 var womenShoe=[
@@ -82,31 +57,7 @@ var womenShoe=[
 				price: "$45.00",
 				infor: "Check all the right boxes with the easy comfort and lightweight design of the Native® Shoes Jefferson 2.0 High shoe! High-top silhouette offers a classic look"
 
-			},{
-				title:"Newton Running Gravity All Weather",
-				path: "images/shoe/womenShoe/newTon.jpg",
-				price: "$80.00",
-				infor: "Stay connected to the ground even in wet weather with the durable and responsive construction of the Newton® Running Gravity All Weather running shoe!"
-
-			},{
-				title:"Nike Air Force 1 Sage High",
-				path: "images/shoe/womenShoe/nikeAir.jpg" ,
-				price: "$70.00",
-				infor: "Playing on the classic silhouette of the high-cut original, the Nike Air Force 1 Sage High takes things up a notch with a platform midsole and a synthetic upper that extends all the way up the calf. Adding to its bold irreverence, removable design details let you tailor the shoe to your own style."
-
-			},{
-				title:"YEEZY BOOST V2 ZEBRA",
-				path: "images/shoe/womenShoe/nikeKid.jpg" ,
-				price: "$40.00",
-				infor: "The Nike Cortez Basic SL Big Kids' Shoe stays true to the original 1972 design. It's built with synthetic leather for durability and lightweight cushioning for comfort."
-			},{
-				title:"Superga 2553 COTU",
-				path: "images/shoe/womenShoe/superGa.jpg" ,
-				price: "$50.00",
-				infor: "This modern reimagining of Superga classics features a high-top silhouette on a chunky platform that features a bold tread."
-
-			}
-		 ]
+			}]
 //set the menSock in an array for easier table create in funciton
 var menSock = [ {	
 				path:"images/shoe/sock/menSock/adidasMen.jpeg" ,
@@ -129,36 +80,10 @@ var menSock = [ {
 				title:"Gold Toe Men's",
 				price:"$4.00",
 				infor:"Get comfort and style out of the sockless look with this 8 pack of Gold Toe no-show socks, with wicking technology to help keep your feet dry plus plush cushioning."
-			},{
-				path:"images/shoe/sock/menSock/Hanes.jpeg" ,
-				title:"Hanes Men's",
-				price:"$5.00",
-				infor:"Help your feet stay fresh with this six pack of socks from Hanes, a low-profile look with improved breathability and moisture-wicking technology for active comfort." 
-			},{
-			
-				path:"images/shoe/sock/menSock/nikeMencotten.jpeg",
-				title:"Nike Men's Socks, Dri Fit Crew",
-				price:"$4.00",
-				infor:"Featuring a snug, compression fit at the arch, the Nike Dri-FIT Low Cut Socks (Medium/6 Pairs) offers a supportive fit that's ultra-comfortable."
-
-			},{
-				path:"images/shoe/sock/menSock/poloRalph.jpeg", 
-				title:"Polo Ralph Lauren Men's Socks",
-				price:"$5.00",
-				infor:"Gear up for the gym in style with these Ralph Lauren socks with signature big pony polo player graphics." 
-
-			},{
-				path:"images/shoe/sock/menSock/tommyHifiger.jpeg", 
-				title:"Tommy Hilfiger Dress Socks",
-				price:"$5.00 ",
-				infor:"Go through the work week in subtle style and total comfort with this 5 pack of Tommy Hilfiger dress socks" 
-
-			}
-			]
+			}]
 
 //set the womenSock in an array for easier table create in funciton
-var womenSock = [
-				{
+var womenSock = [{
 				path:"images/shoe/sock/womenSock/adidasSuperlite.jpeg", 
 				title:"ADIDAS SUPERLITE WOMEN'S NO SHOW SOCKS",
 				price:"$4.00",
@@ -178,27 +103,4 @@ var womenSock = [
 				title:"NEW BALANCE CORE COTTON WOMEN'S NO SHOW SOCKS",
  				price:"$4.00 ",
  				infor:"Stay dry while working up a sweat in the Cotton Core no show socks from New Balance! This 3 pack of socks feature cushioning and support to help you tackle whatever the day brings you in ultimate comfort."
- 				},{
-				path:"images/shoe/sock/womenSock/nikePerformance.jpeg",
-				title:"NIKE PERFORMANCE CUSHIONED WOMEN'S NO SHOW SOCKS",
- 				price:"$5.00",
- 				infor:"These no-show socks from Nike are are your perfect go-to choice when you're prepping for a workout! Dri-Fit technology keeps your feet cool and dry to help improve performance."
- 				},{
-				path:"images/shoe/sock/womenSock/nineWest.jpeg", 
-				title:"NINE WEST TROUSER WOMEN'S CREW SOCKS",
-				price:"$4.00",
-				infor:"Perfect for pairing under your dress pants, the Trouser crew socks from Nine West are lightweight, breathable, and super sleek. "
-				},{
-				path:"images/shoe/sock/womenSock/solidWomen.jpeg",
-				title:"SOLID WOMEN'S NO SHOW SOCKS",
-				price:"$5.00",
-				infor:"These solid black no show socks from Mix No. 6 are are your perfect go-to choice! Never run out of socks with this 6 pack option, and wear them alone or with your favorite sneakers!"
-				},{
-				path:"images/shoe/sock/womenSock/viaSpiga.jpeg",
-				title:"VIA SPIGA MICROFIBER CUSHIONED WOMEN'S NO SHOW SOCKS",
- 				price:"$4.00",
- 				infor:"Stay comfortable in sleek style with the Microfiber Cushioned no show socks from Via Spiga. With a padded footbed and soft microfiber construction, these low-cut socks will keep your style on point."
-
-
-}
-]
+ 				}]
